93042c8b52ea384b6c07cb09485a38043917ba5c
Binay Chaudhary <43121669+Binay0@users.noreply.github.com>
2023-01-23 22:23:04 +1100

    created list DOM

---
+++ script.js
@@ -0,0 +1,51 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById('check');
+
+
+const richestPeople = [
+    "Jeff Bezos",
+    "Bill Gates",
+    "Warren Buffet",
+    "Bernard Arnault",
+    "Carlos Slim Helu",
+    "Amancio Ortega",
+    "Larry Ellison",
+    "Mark Zuckerberg",
+    "Larry Page"
+];
+
+
+//store list items 
+const listItems = [];
+
+
+
+let dragStartIndex;
+
+createList();
+
+
+//Insert List items into DOM
+function createList(){
+    [...richestPeople]
+    .map(a=>({value: a, sort: Math.random()}))
+    .forEach((person, index) =>{
+        const listItem = document.createElement("li");
+
+        listItem.setAttribute('data-index', index);
+
+
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable = "true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>
+
+        `;
+
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+
+    })
+}
